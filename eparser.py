303e46f3fe802f5caaa9a64b589d7f66ac89b37f
Andrej <4andrej@gmail.com>
2022-12-02 20:45:23 +0100

    exporting to html

--- eparser.py
+++ eparser.py
@@ -1,15 +1,45 @@
-import email
-import sys
+import email, email.parser, sys, os
+from prettytable import PrettyTable
 
+
+def header_decode(header):
+    hdr = ""
+    for text, encoding in email.header.decode_header(header):  # type: ignore
+        if isinstance(text, bytes):
+            text = text.decode(encoding or "us-ascii")
+        hdr += text
+    return hdr
+
+
+count = 1
+t = PrettyTable(["C", "OD", "PRE", "PREDMET", "DÁTUM"])
+
+for filename in os.listdir(os.getcwd() + "/data"):
+
+    try:
+        f = open(os.path.join(os.getcwd(), "data/", filename), "r", encoding="utf8")
+
+    except OSError:
+        print("Could not open file...bye")
+        sys.exit()
+
+    with f:
+        msg = email.message_from_file(f)
+
+        frm = header_decode(msg["From"])
+        to = header_decode(msg["To"])
+        sbj = header_decode(msg["Subject"])
+        date = header_decode(msg["Date"])
+        t.add_row([count, frm, to, sbj, date])
+        count += 1
+
+print(t.get_html_string())
 
 try:
-    f = open("./data/email.eml", "r", encoding="utf8")
-
+    out = open(os.path.join(os.getcwd(), "out.html"), "w")
 except OSError:
-    print("Could not open file...bye")
+    print("Could not create out file!")
     sys.exit()
 
-with f:
-    msg = email.message_from_file(f)
-    print(msg["From"])
-    print(msg["To"])
+with out:
+    out.write(t.get_html_string(format=True))
